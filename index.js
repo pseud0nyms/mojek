cabcc99c8ae8337febc9b6c4bb8f9e0abc2c4b6d
Febrian <solihinfebrian0@gmail.com>
2020-06-19 22:15:21 +0700

    Create routes & update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express   = require('express');
 const mongoose  = require('mongoose');
+const customers = require('./routes/costumers');
+const genres    = require('./routes/genres');
+const movies    = require('./routes/movies');
 const app = express();
 
 mongoose.connect('mongodb://localhost/mojek', ({ useNewUrlParser: true,  useUnifiedTopology: true}))
@@ -7,6 +10,9 @@ mongoose.connect('mongodb://localhost/mojek', ({ useNewUrlParser: true,  useUnif
     .catch(err => console.log('Could not connect to MongoDB..'));
 
 app.use(express.json());
+app.use('/api/customers', customers);
+app.use('/api/genres', genres);
+app.use('/api/movies', movies);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
